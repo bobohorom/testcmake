8b7c52bb48f64521e234fccba0be6480c54787c1
bobohorom <bobohorom@gmail.com>
2026-02-13 23:24:09 +0100

    exe dir creation

--- out/build/gcc/CMakeFiles/helloworld.dir/cmake_clean.cmake
+++ out/build/gcc/CMakeFiles/helloworld.dir/cmake_clean.cmake
@@ -1,13 +1,13 @@
 file(REMOVE_RECURSE
+  "C:/dev/cpp/github/testcmake/exe/helloworld.exe"
+  "C:/dev/cpp/github/testcmake/exe/helloworld.exe.manifest"
+  "C:/dev/cpp/github/testcmake/exe/helloworld.pdb"
   "CMakeFiles/helloworld.dir/src/fic2.cpp.obj"
   "CMakeFiles/helloworld.dir/src/fic2.cpp.obj.d"
   "CMakeFiles/helloworld.dir/src/helloworld.cpp.obj"
   "CMakeFiles/helloworld.dir/src/helloworld.cpp.obj.d"
   "CMakeFiles/helloworld.dir/src/testc.c.obj"
   "CMakeFiles/helloworld.dir/src/testc.c.obj.d"
-  "helloworld.exe"
-  "helloworld.exe.manifest"
-  "helloworld.pdb"
   "libhelloworld.dll.a"
 )
 
